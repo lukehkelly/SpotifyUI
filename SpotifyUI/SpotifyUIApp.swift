a01061497e01c4b6e2049cbd80ef6ded3948e0e5
lukehkelly <lhkelly@unc.edu>
2025-09-22 10:50:52 -0400

    Initial Commit

---
+++ SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by Luke Kelly on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
